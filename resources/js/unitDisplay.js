0e1b6c0a3c25510017d27700d7e58c12c7320315
eyedarts <jbh@riseup.net>
2017-08-12 17:22:56 -0400

    Change focus of javascript for main index.html

---
+++ resources/js/unitDisplay.js
@@ -0,0 +1,14 @@
+// Check and set display value of site blocks
+
+function unitDisplay(unitNumber) {
+  unitNumber = "unit" + unitNumber;
+    var unitBox = document.getElementById(unitNumber);
+    
+    if (unitBox.style.display == "" || unitBox.style.display == "none") {
+    unitBox.style.display = "flex";
+    
+  } else {
+    unitBox.style.display = "none";
+  }
+
+}
